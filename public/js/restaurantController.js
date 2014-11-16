c2a3c4df251897d35ae40ca71cc5071ba29ea912
czengg <czengg@CMU-896368.WV.CC.CMU.EDU>
2014-11-16 00:26:56 -0500

    initial commit after git fiasco

---
+++ public/js/restaurantController.js
@@ -0,0 +1,35 @@
+var RestaurantController = function ($http, $scope) {
+
+  this.favoriteToggle = function(restaurant, dish) {
+    dish.favorited = !dish.favorited;
+    var counter;
+    var f;
+    if (counter !== undefined) {
+      clearTimeout(counter);
+    }
+    counter = setTimeout(function() {
+      if (dish.favorited) {
+        f = 1;
+      } else {
+        f = 0;
+      }
+      var favoriteUrl = 'http://localhost:3000/updatefavorite/' + restaurant.id + '/' + dish.id + '/' + f;
+      $http.get(favoriteUrl).success(function(data) {
+        console.log(data);
+      });
+    }, 50);  
+  }
+  
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      self.long = Math.floor(position.coords.longitude);
+      self.lat = Math.floor(position.coords.latitude);
+      var geoUrl = 'http://localhost:3000/getrestaurant/' + self.long + '/' + self.lat;
+      $http.get(geoUrl).success(function(data) {
+        $scope.restaurant = data;
+      });
+    });
+  }
+};    
+
+angular.module('top5').controller('RestaurantController', RestaurantController);
---
+++ server.js
@@ -0,0 +1,141 @@
+
+var http     = require('http'),
+    path     = require('path'),
+    fs       = require('fs'),
+    url      = require('url'),
+    mongo    = require('mongodb'),
+    mongoose = require('mongoose'),
+    express  = require('express'),
+    jade = require('jade');
+
+var app = express();
+var router = express.Router();
+var pub = path.join(__dirname, 'public');
+
+//all environments
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(pub));
+// app.use(passport.initialize());
+// app.use(passport.session());
+app.set('views', pub + '/views');
+app.set('view engine', 'jade');
+
+// establish mongo connection
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/data';
+var db = mongoose.createConnection(mongoUri);
+db.on('error', function(err) {
+  console.log('MongoDB connection error:', err);
+});
+db.once('open', function() {
+  console.log("MongoDB connected");
+});
+
+
+// New mongoose schema to create our
+var Schema = mongoose.Schema;
+var Collection = mongoose.Collection;
+
+var Dish = new Schema({
+  id             : { type: Number , required: true },
+  name           : { type: String , required: true },
+  type           : { type: String , required: true },
+  description    : { type: String , required: true },
+  favorited      : { type: Boolean, required: true },
+  likes          : { type: Number , required: true },
+  restaurant_id  : { type: Number , required: false } 
+});
+var DishModel = db.model('Dish', Dish);
+
+var Restaurant = new Schema({
+  name     : { type: String, required: true },
+  lat      : { type: Number, required: true },
+  long      : { type: Number, required: true },
+});
+var RestaurantModel = db.model('Restaurant', Restaurant);
+
+router.get('/', function (req, res) {
+    res.render('index');
+});
+
+// get all dishes for restaurant given longitude and latitude
+router.get('/getrestaurant/:long/:lat', function(req, res) {
+ RestaurantModel.findOne({
+    long : parseInt(req.params.long),
+    lat  : parseInt(req.params.lat)
+ }, function(restaurantErr, restaurant) {
+
+    if (restaurantErr) { res.send(JSON.stringify(restaurantErr)); }
+
+    var rest = restaurants;
+    DishModel.find({
+      restuarant_id : restaurant._id
+    }, function(dishErr, dishes) {
+
+      if (dishErr) { res.send(JSON.stringify(dishErr)); } 
+
+      rest.dishes = dishes;
+      rest.selected = dishes[0];
+      res.send(JSON.stringify(rest));
+    });
+  });
+});
+
+router.get('/updatefavorite/:restaurant/:dish/:favorite', function(req, res) {
+  DishModel.findOne({
+    id: req.params.dish
+  }, function(err, dish) {
+    if (err) { res.send(err) }
+    if (req.params.dish === 1) {
+      dish.favorited = true;
+      dish.likes += 1;
+    } else {
+      dish.favorited = false;
+      dish.likes -= 1;
+    }
+    dish.save(function(err) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(dish);
+      }
+    });
+  });
+});
+
+router.get('/addrestaurant/:name/:lat/:lon', function(req, res) {
+  var restaurant = new RestaurantModel({
+    name: req.params.name,
+    lat: req.params.lat,
+    lon: req.params.lon
+  });
+  restaurant.save(function(err){
+    if (err) { res.send(err) }
+    res.send(restaurant);
+  });
+});
+
+router.post('/adddish', function(req, res) {
+  RestaurantModel.findOne({
+    name: req.body.restaurant
+  }, function (err, restaurant) {
+    if (err) { res.send(err) }
+    var dish = new DishModel({
+      name: req.body.dish.name,
+      type: req.body.dish.type,
+      description: req.body.dish.description,
+      favorited: req.body.dish.favorited,
+      likes: req.body.dish.likes,
+      restaurant_id: restaurant._id
+    });
+    dish.save(function(err) {
+      if (err) { res.send(err) }
+      res.send(dish);
+    });
+  });
+});
+
+app.use('/', router);
+
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
+});
